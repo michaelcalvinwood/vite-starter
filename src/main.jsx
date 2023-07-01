c6adc064039d15658e348228496906e3a59fd599
Michael Wood <michaelwood33311@icloud.com>
2023-07-01 09:07:06 -0400

    added Chakra UI basic

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// 1. Import the extendTheme function
+import { extendTheme } from '@chakra-ui/react'
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  
+}
+
+const theme = extendTheme({ colors })
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
 )
